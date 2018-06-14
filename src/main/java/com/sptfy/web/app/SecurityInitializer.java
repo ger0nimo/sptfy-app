97c99c0d5c1e7dd0b4ee9d5d743dd832d97af819
grjk <grjk@ailleron.com>
2018-06-14 16:03:29 +0200

    Added spring security initializer and config class

---
+++ src/main/java/com/sptfy/web/app/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.sptfy.web.app;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
