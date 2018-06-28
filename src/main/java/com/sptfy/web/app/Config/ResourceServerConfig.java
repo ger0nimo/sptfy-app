441640502a8f5a51e88342b74d804224dcd30f6f
grjk <grjk@ailleron.com>
2018-06-28 15:28:33 +0200

    H2 DB Access issue fix

--- src/main/java/com/sptfy/web/app/Config/ResourceServerConfig.java
+++ src/main/java/com/sptfy/web/app/Config/ResourceServerConfig.java
@@ -13,9 +13,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
 
                 http
-                .authorizeRequests()
-                        .antMatchers("/h2-console").permitAll()
-                        .antMatchers("/rest/**").authenticated() // * - authentication required for everything after /rest, ** - everything after /rest including /rest
+                .authorizeRequests()                                            //ORDER OF TWO BELOW IS IMPORTANT!
+                        .antMatchers("/h2-console/**").permitAll() //normally this should strictly required authentication
+                        .antMatchers("/**").hasRole("USER") // * - authentication required for everything after /rest, ** - everything after /rest including /rest//
                 .and()
                 .csrf().disable()                       //these two are mandatory to enable
                 .headers().frameOptions().disable();    //H2 Console when Spring Security is configured
